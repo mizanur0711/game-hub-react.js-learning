4fe6aea783f9196a51d90b347b8322261b6f9e4f
mizanur0711 <mizanur0711@gmail.com>
2023-07-26 15:36:01 +0600

    deleted genre prefetched data

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,5 +1,4 @@
 import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
 
