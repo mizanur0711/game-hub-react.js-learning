70274f067ed192164d1defbb54ea27c044c93d65
mizanur0711 <mizanur0711@gmail.com>
2023-07-28 16:18:34 +0600

    Refactor - create lookup hooks

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,7 +1,9 @@
 import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
-import useGenres from "../hooks/useGenre";
+import useGenre from "../hooks/useGenre";
+import useGenres from "../hooks/useGenres";
+import usePlatform from "../hooks/usePlatform";
 import usePlatforms from "../hooks/usePlatforms";
 
 interface Props {
@@ -9,13 +11,8 @@ interface Props {
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: genres } = useGenres();
-  const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
-
-  const { data: platforms } = usePlatforms();
-  const platform = platforms?.results.find(
-    (p) => p.id === gameQuery.platformId
-  );
+  const genre = useGenre(gameQuery.genreId);
+  const platform = usePlatform(gameQuery.platformId);
 
   const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
   return (
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,25 +1,8 @@
+import useGenres from "./useGenres";
 
-import { useQuery } from "@tanstack/react-query";
-//import genres from "../data/genres";
-import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
+const useGenre = (id?: number) => {
+  const { data: genres } = useGenres();
+  return genres?.results.find((g) => g.id === id);
+};
 
-const apiClient = new APIClient<Genre>("/genres");
-
-export interface Genre {
-  id: number;
-  name: string;
-  slug: string;
-  games_count: number;
-  image_background: string;
-}
-
-const useGenres = () =>
-  useQuery({
-    queryKey: ["genres"],
-    queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
-    //  initialData: { count: genres.length, results: genres },
-  });
-
-export default useGenres;
+export default useGenre;
